07ff43bf8e955922fd8d709aa566d0ed493b5096
VitalikApter <aptvetal2014@gmail.com>
2023-03-08 18:10:43 +0200

    registraionScreen, image

--- App.js
+++ App.js
@@ -1,10 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground} from 'react-native';
+import { RegistrationScreen } from "./Screens/RegistrationScreen";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Working!!!!</Text>
+      <ImageBackground
+        source={require("./assets/Back.jpg")}
+        style={styles.image}
+      >
+        <RegistrationScreen />
+        
+      </ImageBackground>
+      <Text>Ты</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +21,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+   
+  },
+  image: {
+    flex: 2,
+    resizeMode: "cover",
+    justifyContent: "center",
   },
 });
---
+++ Screens/RegistrationScreen.js
@@ -0,0 +1,9 @@
+import { View } from "react-native";
+import { Text } from "react-native";
+export const RegistrationScreen = () => {
+  return (
+    <View>
+      <Text>Hello</Text>
+    </View>
+  );
+};
